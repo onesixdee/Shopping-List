4b36b2906e5e34526d764955ca553bf45d4c171c
Diana Duong <dduong013@gmail.com>
2016-02-21 14:29:18 -0600

    updated button css to apply to this button when clicked/unclicked

--- js/cart.js
+++ js/cart.js
@@ -6,48 +6,47 @@ $(document).ready(function() {
 	$('.item-entered').hide();
 
 
-// button CSS style change when clicked
-	function buttonStyle() {
-	    var button = $('.produce-btn');
-	    if (button.hasClass('btn-clicked')) {
-	        button.removeClass('btn-clicked').addClass('btn-unclicked');
+	
+// CSS for buttons when clicked/unclicked
+	$('button').click(function() {
+		if($(this).hasClass('btn-unclicked')) { 
+			$(this).removeClass('btn-unclicked').addClass('btn-clicked');
+		}
+		else if ($(this).hasClass('btn-clicked')) {
+	    	$(this).removeClass('btn-clicked').addClass('btn-unclicked');
 	    }
-	    else if (button.hasClass('btn-unclicked')) {
-	        button.removeClass('btn-unclicked').addClass('btn-clicked');
-	    }
-	      else {
-	        button.addClass('btn-unclicked');
-	    }
-	};
+		else {
+			$(this).addClass('btn-clicked');
+		}
+	});
 
-	 $('.produce-btn').click(function() {
-        buttonStyle();
-    });
+ 
 
 
-// slidetoggle of Add Items
+
+
+
+
+
+
+
+
+
+//slidetoggle of Add Items
 	$('.produce-btn').click(function() {
 		$('#add-produce-item').slideToggle();
 	});
 
-	$('.bakery-btn').click(function() {
-			// $(this).removeClass('btn-unclicked');
-			// $(this).addClass('btn-clicked');
-			$('#add-bakery-item').slideToggle();
+	$('.frozen-btn').click(function() {
+			$('#add-frozen-item').slideToggle();
 	});
 	$('.meat-btn').click(function() {
-			// $(this).removeClass('btn-unclicked');
-			// $(this).addClass('btn-clicked');
 			$('#add-meat-item').slideToggle();
 	});
 	$('.dairy-btn').click(function() {
-			// $(this).removeClass('btn-unclicked');
-			// $(this).addClass('btn-clicked');
 			$('#add-dairy-item').slideToggle();
 	});
 	$('.pantry-btn').click(function() {
-			// $(this).removeClass('btn-unclicked');
-			// $(this).addClass('btn-clicked');
 			$('#add-pantry-item').slideToggle();
 	});
 
@@ -67,16 +66,12 @@ $(document).ready(function() {
 
    
 
-
-
-
-// change css back to white background and black text when button is closed
-
 // slide toggle for item entered
 
 // remove all text in input / clear
 
-// value in item entered inside <p> needs to be inside of <input> (float left?)
+
+
 
 
 
