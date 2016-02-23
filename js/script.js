bc34a8c983d5b3c50c854eb6e0c2768fd1d3b728
Diana Duong <dduong013@gmail.com>
2016-02-23 13:33:06 -0600

    close button to slideup all contents inside button

--- js/script.js
+++ js/script.js
@@ -12,20 +12,23 @@
   	// when the enter key is pressed on .add-items, the appended message displays the checkbox and the input valuefrom .add-items
 	  $('.add-item').keypress(function (e) {
 	    if (e.which == 13) {
-	      $(this).parent().parent().append('<li class="item-entered"><input type="checkbox" class="checkbox">' + $(this).val() + '</input></li>')
+	      $(this).parent().append('<li class="item-entered" id="filter"><input type="checkbox" class="checkbox">' + $(this).val() + '</input></li>')
 	      $(this).val('')
 	    }
   	})
 
 
+	$('.close').click(function () {
+		$('li').find('#filter').slideUp()
+		$('.btn').removeClass('btn-clicked')
+	})
 
-
-
-	  $('.reset').click(function () {
-	  	$('.item-entered').remove()
-	  	$('.add-item').remove() 
-	  	$('.btn').removeClass('btn-clicked')
-	  })
+	  // reset
+	  // $('.reset').click(function () {
+	  // 	$('.item-entered').remove()
+	  // 	$('.add-item').remove() 
+	  // 	$('.btn').removeClass('btn-clicked')
+	  // })
 })
 
 
