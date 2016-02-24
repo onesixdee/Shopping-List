4cf1af099eab32ff81c5c18a6e6455cd84fcf34c
Diana Duong <dduong013@gmail.com>
2016-02-23 19:56:17 -0600

    updated

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@
   	// when the enter key is pressed on .add-items, the appended message displays the checkbox and the input valuefrom .add-items
 	  $('.add-item').keypress(function (e) {
 	    if (e.which == 13) {
-	      $(this).parent().append('<li class="item-entered btn-content"><input type="checkbox" class="checkbox">' + $(this).val() + '</input></li>')
+	      $(this).parent().append('<li class="item-entered btn-content">' + '<span>' + $(this).val() + '</span>' + '<input type="checkbox" class="checkbox"></input></li>')
 	      $(this).val('')
 	    }
   	})
+
 
 	 //close button to hide all contents inside button
 	$('.close').click(function () {
@@ -35,8 +36,7 @@
 
 
 
-
-
+		
 	
 
 
