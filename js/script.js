a75da8bb453d21c7c8f6563847dabbe4c82fd038
Diana Duong <dduong013@gmail.com>
2016-02-26 12:26:12 -0600

    delete button to delete the list item

--- js/script.js
+++ js/script.js
@@ -1,51 +1,56 @@
 
-	$(document).ready(function () {
+$(document).ready(function () {
 
-	  // when a button clicked has a class of btn, toggleClass changes the class to btn-clicked and the siblings of btn (.add-item) is displayed
-	  $('button').click(function () {
-	    if ($(this).hasClass('btn')) {
-	      $(this).toggleClass('btn-clicked')
-	      $(this).siblings().slideToggle()
-	    }
-	  })
-
-  	// when the enter key is pressed on .add-items, the appended message displays the checkbox and the input valuefrom .add-items
-	  $('.add-item').keypress(function (e) {
-	    if (e.which == 13) {
-	      $(this).parent().append('<li class="item-entered btn-content">' + '<span>' + $(this).val() + '</span><input type="checkbox"></input></li>')
-	      $(this).val('')
-	    }
-  	})
+	// click category buttons and toggle input for add items
+		$('button').click(function () {
+	    	if ($(this).hasClass('btn')) {
+	      			$(this).toggleClass('btn-clicked')
+	      			$(this).siblings().slideToggle()
+	    	}
+	  	})
 
 
-          		/* strikethrough text if checkbox selected */
-	$('#categories').on('change', 'input:checkbox',function () {
-		var item = $(this).closest('li').find('span')
-		if($(this).is(':checked')) {
-			item.addClass('item-found').append('<button class="remove">remove</button>')
-			$(this).closest('input:checkbox').remove()
-		}
-		else{
-			item.removeClass('item-found')
-			
-		}
-	})
+  	// use enter to add items
+	 	$('.add-item').keypress(function (e) {
+	    	if (e.which == 13) {
+	      		$(this).parent().append('<li class="item-entered btn-content">' + '<span>' + $(this).val() + '</span><input type="checkbox"></input></li>')
+	      		$(this).val('')
+	    	}
+		})
 
-      
-	  // $('#categories').sortable();
 
-	 //close button to hide all contents inside button
-	$('.close').click(function () {
-		$('li').find('.btn-content').slideUp()
-		$('.btn').removeClass('btn-clicked')
-	})
+	 //check items off list
+		$('#categories').on('change', 'input:checkbox',function () {
+			var item = $(this).closest('li').find('span')
+			if($(this).is(':checked')) {
+				item.addClass('item-found')
+				$(this).closest('.item-entered').append('<button class="delete">delete</button>')
+				$(this).closest('input:checkbox').remove()
+			}
+			else {
+				item.removeClass('item-found')
+			}
+		})
 
-	  // reset
-	  $('.reset').click(function () {
-	  	$('.item-entered').remove()
-	  	$('.add-item').slideUp() 
-	  	$('.btn').removeClass('btn-clicked')
-	  })
+	// delete items
+		$('#categories').on('click','.delete', function(){
+			$(this).parent().remove()
+		})
+
+
+	//close button to hide all contents inside button
+		$('.close').click(function () {
+			$('li').find('.btn-content').slideUp()
+			$('.btn').removeClass('btn-clicked')
+		})
+
+
+	// reset
+	  	$('.reset').click(function () {
+	  		$('.item-entered').remove()
+	  		$('.add-item').slideUp() 
+	  		$('.btn').removeClass('btn-clicked')
+	  	})
 })
 
 
